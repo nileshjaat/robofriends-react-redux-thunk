8e178641a4b2ba0735d3cf5e95e2777d9e00497d
Nilesh <nileshjaat341@gmail.com>
2020-06-27 18:30:46 +0530

    Updates

---
+++ precache-manifest.7424eed6d01861776fed04cee7b9a7a0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e93980dc2651afc91c9507a729fcb0a",
+    "url": "/robofriends-react-redux-thunk/index.html"
+  },
+  {
+    "revision": "87266d219e52705cfead",
+    "url": "/robofriends-react-redux-thunk/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "65b2f0250ea8ff2ae16e",
+    "url": "/robofriends-react-redux-thunk/static/css/main.6243cc36.chunk.css"
+  },
+  {
+    "revision": "87266d219e52705cfead",
+    "url": "/robofriends-react-redux-thunk/static/js/2.55a3d9d3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/robofriends-react-redux-thunk/static/js/2.55a3d9d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65b2f0250ea8ff2ae16e",
+    "url": "/robofriends-react-redux-thunk/static/js/main.ab8cf746.chunk.js"
+  },
+  {
+    "revision": "702c1dc0bea7f160b65d",
+    "url": "/robofriends-react-redux-thunk/static/js/runtime-main.c38b1ed8.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-react-redux-thunk/static/media/SEGA.12a3db8b.woff"
+  }
+]);
